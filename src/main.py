6b128d7848f4f6185609e65fb9c38baf3497cc85
Damian34 <dam.wadolowski@gmail.com>
2026-05-07 13:27:34 +0200

    add data enrichment

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from src.claim_analyzer import ClaimAnalyzer
 from src.source_loader import SourceLoader
 from src.claim_normalizer import ClaimNormalizer
 
@@ -5,6 +6,8 @@ from src.claim_normalizer import ClaimNormalizer
 if __name__ == '__main__':
    loader = SourceLoader()
    normalizer = ClaimNormalizer()
+   analyzer = ClaimAnalyzer()
    for data in loader.get_insurance_claims_data():
       normalized_data = normalizer.normalize(data)
-      print(normalized_data)
+      enriched_data = analyzer.calculate_and_enrich(normalized_data)
+      print(enriched_data)
